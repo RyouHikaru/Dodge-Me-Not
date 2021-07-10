73daa889ae09ffa290c1b109d301874a8c12b6ea
Ryou Hikaru <carljamesreblora13@gmail.com>
2021-07-10 13:26:34 +0800

    minimized db_configurations.py

--- db_configurations.py
+++ db_configurations.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+# CREATE TABLE
 def create_table(conn, create_table_sql):
     try:
         c = conn.cursor()
@@ -8,6 +9,7 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+# CREATE CONNECTION TO DATABASE
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
@@ -19,18 +21,20 @@ def create_connection(db_file):
     
     return conn
 
+# INSERT
 def insert_score(conn, record):
     sql = '''INSERT INTO game_records(player_name, level, score)
              VALUES(?, ?, ?)'''
     cur = conn.cursor()
     cur.execute(sql, record)
     conn.commit()
+
     return cur.lastrowid
 
+# SELECT / VIEW
 def query_scores(conn):
     cur = conn.cursor()
     cur.execute("SELECT player_name, level, score FROM game_records ORDER BY score DESC LIMIT 10")
-
     rows = cur.fetchall()
 
     return rows
@@ -42,19 +46,3 @@ create_table_sql = """CREATE TABLE IF NOT EXISTS game_records (
                         level INTEGER,
                         score INTEGER
                     );"""
-
-def main():
-    create_table_sql = """CREATE TABLE IF NOT EXISTS game_records (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            player_name TEXT,
-                            level INTEGER,
-                            score INTEGER
-                        );"""
-
-    conn = create_connection(db_file)
-
-    if conn is not None:
-        create_table(conn, create_table_sql)
-        conn.close()
-    else:
-        print("Cannot create DB connection.")
